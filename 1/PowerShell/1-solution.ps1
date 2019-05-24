3278e512720f3c20e4de83d51a45b1d433cdd5a4
Matt Hale <matthew.hale@protonmail.com>
2019-05-24 06:11:15 -0400

    solved in powershell

--- 1/PowerShell/1-solution.ps1
+++ 1/PowerShell/1-solution.ps1
@@ -1,1 +1,19 @@
+param (
+	[int[]]$Multiples = @(3,5),
+	[int]$Start = 1,
+	[int]$End = 999
+)
+
+$Digits = [System.Collections.Generic.List[int]]::new()
+
+for ( $i = $Start; $i -le $End; $i++ ) {
+	foreach ( $ThisMultiple in $Multiples ) {
+		if ( $i % $ThisMultiple -eq 0 ) {
+			$Digits.Add($i)
+			break
+		}
+	}
+}
+
 $Digits | Measure-Object -Sum | Select -ExpandProperty Sum
+
