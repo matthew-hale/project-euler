138d46ff3e3674b0081573759f3cf9e050900dc6
Matt Hale <matthew.hale@protonmail.com>
2019-09-12 15:26:37 -0400

    Implement sieve of eratosthenes function

--- Problems/51/go/main.go
+++ Problems/51/go/main.go
@@ -2,25 +2,34 @@ package main
 
 //Generate primes using the sieve of eratosthenes method
 //Estimated maximum of 7 digits for this problem
-func eratosthenes(max int) (primes []bool) {
-    primes = make([]bool, max)
+func eratosthenes(max int) []int {
+    primes := make([]bool, max+1)
     for i := range(primes){primes[i] = true}
     p := 2
     for p * p <= max {
         if primes[p] == true {
+            for i := p * 2; i <= max+1; i += p {
+                primes[i] = false
+            }
+        }
+        p++
+    }
+    primes[0] = false
+    primes[1] = false
+
+    var result []int
+    for num := 1; num <= max; num++ {
+        if primes[num] {
+            result = append(result, num)
         }
     }
 
-    return
+    return result
 }
 
 func main() {
-    primes := eratosthenes(10)
-    for i := range(primes){
-        if primes[i] == true {
-            print("true")
-        } else {
-            print("false")
-        }
+    primes := eratosthenes(100)
+    for i := range primes {
+        println(primes[i])
     }
 }
