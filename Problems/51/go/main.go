a30b6a639f49ea548b101ed97cf1ce6dbb4d39dd
Matt Hale <matthew.hale@protonmail.com>
2019-09-12 15:40:29 -0400

    Add comments

--- Problems/51/go/main.go
+++ Problems/51/go/main.go
@@ -3,10 +3,16 @@ package main
 //Generate primes using the sieve of eratosthenes method
 //Estimated maximum of 7 digits for this problem
 func eratosthenes(max int) []int {
+    //Create a slice of booleans with indexes from 0-max, and make them all true
     primes := make([]bool, max+1)
     for i := range(primes){primes[i] = true}
+
+    //Set initial value of p
     p := 2
+
     for p * p <= max {
+        //If the integer is not already marked, loop through its multiples and mark them
+        //Then, increment p
         if primes[p] == true {
             for i := p * 2; i <= max; i += p {
                 primes[i] = false
@@ -14,9 +20,12 @@ func eratosthenes(max int) []int {
         }
         p++
     }
+
+    //As this is usually done with 2 as a minimum value, set 0 and 1 to false
     primes[0] = false
     primes[1] = false
 
+    //Iterate over booleans, adding the indices which are true to the list of primes
     var result []int
     for num := 1; num <= max; num++ {
         if primes[num] {
