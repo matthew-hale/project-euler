50d3e08d913a2ed335428a200806ae66a774bd9e
Matt Hale <matthew.hale@protonmail.com>
2019-09-13 08:36:39 -0400

    Clean up code for test compilation

--- Problems/51/go/main.go
+++ Problems/51/go/main.go
@@ -6,42 +6,41 @@ import (
 
 //Generate primes using the sieve of eratosthenes method
 //Estimated maximum of 7 digits for this problem
-func eratosthenes(max int) []int {
+func eratosthenes(max int) (primes []int) {
     //Create a slice of booleans with indexes from 0-max, and make them all true
-    primes := make([]bool, max+1)
-    for i := range(primes){primes[i] = true}
+    primeBooleans := make([]bool, max+1)
+    for i := range(primeBooleans){primeBooleans[i] = true}
 
     //Set initial value of p
     p := 2
 
+    //As this is usually done with 2 as a minimum value, set 0 and 1 to false
+    primeBooleans[0] = false
+    primeBooleans[1] = false
+
     for p * p <= max {
         //If the integer is not already marked, loop through its multiples and mark them
         //Then, increment p
-        if primes[p] == true {
+        if primeBooleans[p] == true {
             for i := p * 2; i <= max; i += p {
-                primes[i] = false
+                primeBooleans[i] = false
             }
         }
         p++
     }
 
-    //As this is usually done with 2 as a minimum value, set 0 and 1 to false
-    primes[0] = false
-    primes[1] = false
-
     //Iterate over booleans, adding the indices which are true to the list of primes
-    var result []int
     for num := 1; num <= max; num++ {
-        if primes[num] {
-            result = append(result, num)
+        if primeBooleans[num] {
+            primes = append(primes, num)
         }
     }
 
-    return result
+    return
 }
 
 //Generate families for each prime, by replacing digits
-func primeFamilies(x int) [][]int {
+func primeFamilies(x int) bool {
     //Convert the int to a string, then a slice of ints
     str := strconv.Itoa(x)
     var ints []int
@@ -51,6 +50,7 @@ func primeFamilies(x int) [][]int {
     }
 
     //Do digit replacement for all possible combinations
+    return true
 }
 
 func main() {
