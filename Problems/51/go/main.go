94643a169f13a51efd6d8561bdc17741ea224661
Matt Hale <matthew.hale@protonmail.com>
2019-09-26 15:17:19 -0400

    Add some masks to main

--- Problems/51/go/main.go
+++ Problems/51/go/main.go
@@ -6,6 +6,136 @@ import (
     "os"
 )
 
+/*
+00000000
+00000001
+00000010
+00000011
+00000100
+00000101
+00000110
+00000111
+00001000
+00001001
+00001010
+00001011
+00001100
+00001101
+00001110
+00001111
+00010000
+00010001
+00010010
+00010011
+00010100
+00010101
+00010110
+00010111
+00011000
+00011001
+00011010
+00011011
+00011100
+00011101
+00011110
+00011111
+00100000
+00100001
+00100010
+00100011
+00100100
+00100101
+00100110
+00100111
+00101000
+00101001
+00101010
+00101011
+00101100
+00101101
+00101110
+00101111
+00110000
+00110001
+00110010
+00110011
+00110100
+00110101
+00110110
+00110111
+00111000
+00111001
+00111010
+00111011
+00111100
+00111101
+00111110
+00111111
+01000000
+01000001
+01000010
+01000011
+01000100
+01000101
+01000110
+01000111
+01001000
+01001001
+01001010
+01001011
+01001100
+01001101
+01001110
+01001111
+01010000
+01010001
+01010010
+01010011
+01010100
+01010101
+01010110
+01010111
+01011000
+01011001
+01011010
+01011011
+01011100
+01011101
+01011110
+01011111
+01100000
+01100001
+01100010
+01100011
+01100100
+01100101
+01100110
+01100111
+01101000
+01101001
+01101010
+01101011
+01101100
+01101101
+01101110
+01101111
+01110000
+01110001
+01110010
+01110011
+01110100
+01110101
+01110110
+01110111
+01111000
+01111001
+01111010
+01111011
+01111100
+01111101
+01111110
+01111111
+*/
 //Generate primes using the sieve of eratosthenes method
 //Estimated maximum of 7 digits for this problem
 func eratosthenes(max int) (primeBooleans []bool) {
@@ -36,7 +166,7 @@ func eratosthenes(max int) (primeBooleans []bool) {
 }
 
 //Generate families for each prime, by replacing digits
-func primeFamilies(x int) bool {
+func primeFamilies(x int) (families [][]int) {
     //Convert the int to a string, then a slice of ints
     str := strconv.Itoa(x)
     var ints []int
@@ -45,34 +175,126 @@ func primeFamilies(x int) bool {
         ints = append(ints, digit)
     }
 
-    //Do digit replacement for all possible combinations
     /*
-    First, we have to determine all the possible digit replacement combinations we can perform.
-    This is dependent upon the number of digits in the prime.
-    For a 5 digit number, we have to get the binomial coefficient of all nCk values:
-        5 choose 1
-        5 choose 2
-        5 choose 3
-        5 choose 4
-        5 choose 5
-    Then we have to actually get those combinations.
-        e.g. for 5 choose 2, there are 10 combinations; we have to generate all 10, ensuring there are no duplicates
-    The easiest way to do this is via binary counting; counting up through all the 5-digit binary numbers will produce
-    a set of masks for all of our digit replacements for 5 digit numbers; same for 6, 7, etc.
+    The masks are hard-coded here, but we'll implement a function
+    later to generate them.
+
+    Masks are indexed as such:
+
+        The number of digits minus 1 (3 digit masks, x == 2)
+              |
+              |
+              v
+        masks[x][y]
+                 ^
+                 |
+                 |
+        The index of each digit in the mask, continuously, from the rightmost digit (0 == 1's place).
+
+    For performance reasons, we use a 2d slice, and just stack up all the masks back-to-back.
+    We know the width of each mask, so this works out nicely.
+
+    A true value means the digit is to be replaced, while a false value means it stays as is.
     */
-    n := len(ints)
-    for i := 1; i <= n; i++ {
-        binomial := big.Binomial(n,i)
-        for j := 1; j <= binomial; j++ {
-            //binomial = number of combinations
-            //i = number of digits we are choosing
-            //n = total number of digits in the prime
-            //we want a slice of bools to represent a pattern
-            var pattern []bool
-        }
+
+    var masks [][]bool
+    //Single digit doesn't need a mask
+    masks[0][0] = true
+
+    //2 digits, 3 total masks (2 types of single digit replacement, 1 type of double)
+    masks[1] = {
+        true, false,
+        false, true,
+        true, true,
     }
 
-    return true
+    //3 digits
+    masks[2] = {
+        true, false, false,
+        false, true, false,
+        false, false, true,
+        true, true, false,
+        true, false, true,
+        false, true, true,
+        true, true, true,
+    }
+
+    //4 digits
+    masks[3] = {
+        false, false, false, true,
+        false, false, true, false,
+        false, true, false, false,
+        true, false, false, false,
+        false, false, true, true,
+        false, true, false, true,
+        false, true, true, false,
+        true, false, false, true,
+        true, false, true, false,
+        true, true, false, false,
+        false, true, true, true,
+        true, false, true, true,
+        true, true, false, true,
+        true, true, true, false,
+        true, true, true, true,
+    }
+
+    //5 digits
+    masks[4] = {
+        false, false, false, false, true,
+        false, false, false, true, false,
+        false, false, false, true, true,
+        false, false, true, false, false,
+        false, false, true, false, true,
+        false, false, true, true, false,
+        false, false, true, true, true,
+        false, true, false, false, false,
+        false, true, false, false, true,
+        false, true, false, true, false,
+        false, true, false, true, true,
+        false, true, true, false, false,
+        false, true, true, false, true,
+        false, true, true, true, false,
+        false, true, true, true, true,
+        true, false, false, false, false,
+        true, false, false, false, true,
+        true, false, false, true, false,
+        true, false, false, true, true,
+        true, false, true, false, false,
+        true, false, true, false, true,
+        true, false, true, true, false,
+        true, false, true, true, true,
+        true, true, false, false, false,
+        true, true, false, false, true,
+        true, true, false, true, false,
+        true, true, false, true, true,
+        true, true, true, false, false,
+        true, true, true, false, true,
+        true, true, true, true, false,
+        true, true, true, true, true,
+    }
+
+    //6 digits
+    masks[5] = {
+    }
+
+    //Do digit replacement for all possible combinations.
+    var families [][]int
+
+    //Pick the mask based on the length of the integer
+    switch length := len(str); length {
+        case 1:
+            for i := 0; i < 10; i++ {
+                families[0][i] = i+1
+            }
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+    }
+
+    return
 }
 
 func main() {
